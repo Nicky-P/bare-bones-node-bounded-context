41a04ae7448e7816727b84f2a2bef1f00882d386
Nick Perin <nickgperin@gmail.com>
2021-02-26 16:50:44 -0500

    Update helper to go through each prop and check for Model type

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -2,8 +2,17 @@ import * as D from 'io-ts/lib/Decoder';
 import { Response } from 'express';
 import { Model } from 'sequelize';
 
-export const assignDbResValidatorValues = (res: Response, data: Model | Model[], decoder: D.Decoder<unknown, object>) => {
-  res.locals.responseData = data instanceof Model ? data.get({ plain: true }) : data;
+interface SimpleObject {
+  [key: string]: any;
+}
+
+export const assignDbResValidatorValues = (res: Response, data: SimpleObject, decoder: D.Decoder<unknown, object>) => {
+  const formatedData: SimpleObject = {};
+  for (const property in data) {
+    formatedData[property] = data[property] instanceof Model ? data[property].get({ plain: true }) : data[property];
+  }
+
+  res.locals.responseData = formatedData;
   res.locals.decoder = decoder;
 };
 
